07381a78b2ea299adede3e8988dc397e0c888e31
Tomas Alvarez <tomas@vauxoo.com>
2023-12-04 23:28:52 -0600

    Add settings to deploy in fly.io, Use API URL from environment vars

--- src/Common.js
+++ src/Common.js
@@ -1,8 +1,7 @@
-const API_URL = "http://192.168.68.122:8000/tasks/"
+const API_URL = process.env.REACT_APP_API_URL + "/tasks/";
 
 function getApiUrl(path) {
     const url = (API_URL + path + "/").replaceAll("//", "/").replaceAll(":/", "://");
-    console.log(url);
     return url;
 }
 
